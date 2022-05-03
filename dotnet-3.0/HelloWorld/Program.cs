d2ce967e4dfadd3b1ebc757a567cc64028d37ccf
fadoua <fadelhannachi@gmail.com>
2022-05-03 10:20:07 +0200

    modification du hello world

--- dotnet-3.0/HelloWorld/Program.cs
+++ dotnet-3.0/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World again!");
+            Console.WriteLine("Hello World encore!");
 
             Console.ReadLine();
         }
